c186ecdd5a90b60ec5ea1535215c8812fdbe10ae
Nick-Ballenger <nickster214@gmail.com>
2019-12-10 14:21:34 -0700

    initial commit

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sys
-
+print("Let's do this!")
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
